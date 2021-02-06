6737656bdc97da7cabc53237889d7f1690df3ae0
Madoka1224 <lemon1919@protonmail.com>
2021-02-06 12:41:00 +0800

    Add

---
+++ microsoft/collections.py
@@ -0,0 +1,18 @@
+# dictionary
+Willchou = {}
+Willchou['first'] = 'Will'
+Willchou['last'] = 'Chou'
+Suan = {'first': 'Suan', 'last': 'Eric'}
+# print(Willchou)
+# print(Suan)
+people = [Willchou, Suan]
+people.append({'first': 'Bill', 'last': 'Gates'})
+presenters = people[0:2]
+print(people)
+print('-' * 10)
+print(presenters)
+
+array = [0, 23, 34, 46, 67, 21, 12]
+print(array[0:3])
+print(array[2:4])
+print(array[4:5])
